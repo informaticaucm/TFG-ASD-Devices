04ba6cf59eeff540f92887c92f0cd1d2901bd280
jaime gonzalez fabregas <jaimegonzalezfabregas@gmail.com>
2023-12-02 19:40:57 +0100

    provisioning app upgrade

--- provisioning_app/src/index.js
+++ provisioning_app/src/index.js
@@ -2,12 +2,12 @@ const QRCode = require('qrcode')
 
 window.addEventListener('load', function () {
 
-    let conf_defaults = { "thingsboard_url":"https://thingsboads.asd:8080","device_name": "name_here", "mqtt_broker_url": "mqtts://thingsboard.asd:8883", "provisioning_device_key": "o7l9pkujk2xgnixqlimv", "provisioning_device_secret": "of8htwr0xmh65wjpz7qe", "wifi_psw": "1234567890", "wifi_ssid": "tfgseguimientodocente" };
+    let conf_defaults = { "thingsboard_url": "https://thingsboads.asd:8080", "device_name": "name_here", "mqtt_broker_url": "mqtts://thingsboard.asd:8883", "provisioning_device_key": "o7l9pkujk2xgnixqlimv", "provisioning_device_secret": "of8htwr0xmh65wjpz7qe", "wifi_psw": "1234567890", "wifi_ssid": "tfgseguimientodocente" };
     for (const key in conf_defaults) {
         const input = this.document.getElementById(key);
         input.value = conf_defaults[key];
 
-        input.addEventListener("input", () => {
+        const refresh_qr = () => {
             conf = {}
             for (const key in conf_defaults) {
                 const input = this.document.getElementById(key);
@@ -16,7 +16,9 @@ window.addEventListener('load', function () {
 
             let qr_payload = `reconf` + JSON.stringify(conf)
             set_text(qr_payload);
-        })
+        }
+
+        input.addEventListener("input", refresh_qr)
     }
 
 
