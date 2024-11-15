f81a67e5c9fcdaab9d674f3c311b6721fb7c474e
Jack <99360106+Jackknife95@users.noreply.github.com>
2024-11-15 13:21:43 +0100

    Cambiadas las versiones de dependencias
    LVGL= 8 o superior
    esp32_s3_eye ==3.0.4
    
    debido a cambio en librerías de versiones posteriores, ya se han actualizado las dependencias de \source
    
    Especificado en source\util_script\docker_compile.bat que la versión de esp_idf usada es la v5.3.1

--- source/util_scripts/docker_compile.bat
+++ source/util_scripts/docker_compile.bat
@@ -1,2 +1,3 @@
 @REM docker run --rm -v .:/project -w /project -e HOME=/tmp espressif/idf idf.py fullclean
-docker run --rm -v .:/project -w /project -e HOME=/tmp espressif/idf idf.py build
+docker run --rm -v .:/project -w /project -e HOME=/tmp espressif/idf:v5.3.1 idf.py build
+@REM Seteada la versión a v5.3.1 de esp-idf
